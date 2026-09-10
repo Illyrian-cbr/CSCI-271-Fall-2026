14d7fd4fcaeb0cd41947e76faeb01a0c8be5039c
Eris Vokshi <evokshi07@gmail.com>
2026-09-09 21:56:39 -0400

    Assignment 2 mini lab

---
+++ assignment2.cpp
@@ -0,0 +1,22 @@
+// Eris Vokshi
+// Assignment 2
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+string name;
+int age;
+
+cout << "Hey user, what is your name?" << endl;
+cin >> name;
+
+cout << "How old are you?" << endl;
+cin >> age;
+
+cout << "Hi, " << name << "! you are " << age << " years old" << endl;
+
+return 0;
+}
